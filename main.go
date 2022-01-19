f531a14b19920a0a667488ce28e31d1718525a2e
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-19 23:58:41 +0900

    ラベルを指定してforループを抜ける

--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	f := later()
+// 	fmt.Println(f("Golang"))
+// 	fmt.Println(f("is"))
+// 	fmt.Println(f("awesome!"))
+// 	fmt.Println(f(""))
+// }
+
+// func later() func(string) string {
+// 	var store string
+
+// 	return func(next string) string {
+// 		s := store
+// 		store = next
+// 		return s
+// 	}
+// }
+
+// ラベル
 func main() {
-	f := later()
-	fmt.Println(f("Golang"))
-	fmt.Println(f("is"))
-	fmt.Println(f("awesome!"))
-	fmt.Println(f(""))
-}
-
-func later() func(string) string {
-	var store string
-
-	return func(next string) string {
-		s := store
-		store = next
-		return s
+LOOP:
+	for {
+		for {
+			for {
+				fmt.Println("Three nest!")
+				break LOOP
+			}
+		}
 	}
+	fmt.Println(("完了"))
 }
