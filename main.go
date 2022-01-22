8cb37da4aa93a456da3ba3b00921f50234bcc167
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-22 18:28:27 +0900

    配列もポインタ型で渡す

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	i := 1
-	fmt.Println(i)
-	inc(&i)
-	inc(&i)
-	inc(&i)
-	fmt.Println(i)
+func pow(p *[3]int) {
+	i := 0
+	for i < 3 {
+		(*p)[i] = (*p)[i] * (*p)[i]
+		i++
+	}
 }
 
-func inc(p *int) {
-	*p++
+func main() {
+	p := [3]int{1, 2, 3}
+	fmt.Println(p)
+	pow(&p)
+	fmt.Println(p)
 }
+
+// func main() {
+// 	i := 1
+// 	fmt.Println(i)
+// 	inc(&i)
+// 	inc(&i)
+// 	inc(&i)
+// 	fmt.Println(i)
+// }
+
+// func inc(p *int) {
+// 	*p++
+// }
 
 // func main() {
 // 	var i int
