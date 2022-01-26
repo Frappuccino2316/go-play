0c9bd6d3286f15a27fd2bdbdd6f8fc22025e2142
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-27 00:07:56 +0900

    サーバーを起動

--- main.go
+++ main.go
@@ -2,131 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
-	ch := make(chan int)
-
-	go receiver(ch)
-
-	i := 0
-	for i < 10000 {
-		ch <- i
-		i++
-		time.Sleep(time.Second)
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: &handler,
 	}
+	server.ListenAndServe()
 }
-
-func receiver(ch <-chan int) {
-	for {
-		i := <-ch
-		fmt.Println(i)
-	}
-}
-
-// func main() {
-// 	fmt.Println("First Process")
-// 	goto L
-// 	fmt.Println("Second Process")
-// L:
-// 	fmt.Println("Third Process")
-// }
-
-// func pow(p *[3]int) {
-// 	i := 0
-// 	for i < 3 {
-// 		p[i] = p[i] * p[i]
-// 		i++
-// 	}
-// }
-
-// func main() {
-// 	p := [3]int{1, 2, 3}
-// 	fmt.Println(p)
-// 	pow(&p)
-// 	fmt.Println(p)
-// }
-
-// func main() {
-// 	i := 1
-// 	fmt.Println(i)
-// 	inc(&i)
-// 	inc(&i)
-// 	inc(&i)
-// 	fmt.Println(i)
-// }
-
-// func inc(p *int) {
-// 	*p++
-// }
-
-// func main() {
-// 	var i int
-// 	p := &i
-// 	i = 3
-// 	fmt.Println(*p)
-
-// 	*p = 10
-// 	fmt.Println(i)
-// }
-
-// func main() {
-// 	var i int
-// 	p := &i
-// 	pp := &p
-// 	fmt.Println(p)
-// 	fmt.Printf("%T\n", p)
-// 	fmt.Println(pp)
-// 	fmt.Printf("%T\n", pp)
-// }
-
-// map
-// func main() {
-// 	m := make(map[string]int)
-// 	m["one"] = 1
-// 	m["two"] = 2
-// 	fmt.Println(m)
-// }
-
-// スライス
-// func main() {
-// 	// n := make([]int, 5, 10)
-// 	n := []int{1, 2, 3}
-// 	n = append(n, 4, 5, 6)
-// 	fmt.Println("n: ", n)
-// 	fmt.Println(len(n))
-// 	fmt.Println(cap(n))
-// }
-
-// func main() {
-// 	f := later()
-// 	fmt.Println(f("Golang"))
-// 	fmt.Println(f("is"))
-// 	fmt.Println(f("awesome!"))
-// 	fmt.Println(f(""))
-// }
-
-// func later() func(string) string {
-// 	var store string
-
-// 	return func(next string) string {
-// 		s := store
-// 		store = next
-// 		return s
-// 	}
-// }
-
-// ラベル
-// func main() {
-// LOOP:
-// 	for {
-// 		for {
-// 			for {
-// 				fmt.Println("Three nest!")
-// 				break LOOP
-// 			}
-// 		}
-// 	}
-// 	fmt.Println(("完了"))
-// }
